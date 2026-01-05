e13227692fedc195466f5b0abc9a668cf243b6a0
012e <huyphmnhat@gmail.com>
2026-01-05 16:21:40 +0700

    remove pipeline

--- pipeline/config/temporal.py
+++
@@ -1,5 +0,0 @@
-import os
-
-TEMPORAL_HOST = os.getenv("TEMPORAL_HOST", "localhost:7233")
-TEMPORAL_NAMESPACE = os.getenv("TEMPORAL_NAMESPACE", "default")
-TEMPORAL_TASK_QUEUE = os.getenv("TEMPORAL_TASK_QUEUE", "snippet-ingestion-queue")
