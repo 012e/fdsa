90e44d9d1a3e90146c8373802b86edcabc75785b
012e <huyphmnhat@gmail.com>
2026-01-05 17:55:16 +0700

    add spring ai

--- src/main/java/huyphmnat/fdsa/snippet/exceptions/DuplicateSnippetPathException.java
+++
@@ -1,9 +0,0 @@
-package huyphmnat.fdsa.snippet.exceptions;
-
-public class DuplicateSnippetPathException extends RuntimeException {
-
-    public DuplicateSnippetPathException(String path) {
-        super("A snippet with path '" + path + "' already exists");
-    }
-}
-
